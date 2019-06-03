603cad3584e253c9d00851b65676e67b59380ce5
Eukris Songahid <songahid-e@cyscorpions.com>
2019-06-03 16:26:18 +0800

    Final commit

---
+++ app/src/main/java/com/example/lesson73/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.lesson73;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CustomReceiver mReceiver = new CustomReceiver();
+
+    private static final String ACTION_CUSTOM_BROADCAST =
+            BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+
+        this.registerReceiver(mReceiver, filter);
+
+        LocalBroadcastManager.getInstance(this).registerReceiver
+                (mReceiver, new IntentFilter(ACTION_CUSTOM_BROADCAST));
+    }
+
+    /**
+     * Click event handler for the button, that sends custom broadcast using the
+     * LocalBroadcastManager.
+     */
+    public void sendCustomBroadcast(View view) {
+        Intent customBroadcastIntent = new Intent(ACTION_CUSTOM_BROADCAST);
+        LocalBroadcastManager.getInstance(this)
+                .sendBroadcast(customBroadcastIntent);
+    }
+
+    /**
+     * Unregisters the broadcast receivers when the app is destroyed.
+     */
+    @Override
+    protected void onDestroy() {
+        this.unregisterReceiver(mReceiver);
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mReceiver);
+        super.onDestroy();
+    }
+}
